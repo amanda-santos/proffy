c2bb59d5f978555b0c1b3bea6953d2ebe3bdede8
Amanda Santos <amandasf0200@gmail.com>
2020-08-07 21:26:13 -0300

    backend and frontend (partial)

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,27 +1,48 @@
 import React from "react";
 
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem from "../../components/TeacherItem";
 
 import "./styles.css";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Horário</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Música", label: "Música" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-Feira" },
+              { value: "2", label: "Terça-Feira" },
+              { value: "3", label: "Quarta-Feira" },
+              { value: "4", label: "Quinta-Feira" },
+              { value: "5", label: "Sexta-Feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+          <Input type="time" name="time" label="Horário" />
         </form>
       </PageHeader>
 
